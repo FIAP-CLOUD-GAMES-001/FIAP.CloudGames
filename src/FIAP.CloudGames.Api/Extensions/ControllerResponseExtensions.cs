2dde9889129b795384388abff0db0b153b79d06f
lucas <lucas@LUCASLEMOS>
2025-06-18 19:58:15 -0300

    Configurando response para endpoints e middleware para tratativa de erros.

---
+++ src/FIAP.CloudGames.Api/Extensions/ControllerResponseExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FIAP.CloudGames.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FIAP.CloudGames.Api.Extensions;
+public static class ControllerResponseExtensions
+{
+    public static IActionResult ApiOk<T>(this ControllerBase controller, T data, string? message = null)
+    {
+        return controller.Ok(ApiResponse<T>.Ok(data, message));
+    }
+
+    public static IActionResult ApiFail(this ControllerBase controller, string message, List<string>? errors = null, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    {
+        var response = ApiResponse<string>.Fail(message, errors);
+        return controller.StatusCode((int)statusCode, response);
+    }
+
+    public static IActionResult ApiFail(this ControllerBase controller, string message, List<string>? errors = null, int statusCode = 400)
+    {
+        var response = ApiResponse<string>.Fail(message, errors);
+        return controller.StatusCode(statusCode, response);
+    }
+}
