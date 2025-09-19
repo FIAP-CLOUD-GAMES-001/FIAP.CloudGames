c5cc409f7f06c691647079293fc121746afee232
lucas <lucas@LUCASLEMOS>
2025-09-18 22:35:20 -0300

    Melhorando logs.

--- src/ConvertTextInAudio/ConvertTextInAudio/Worker.cs
+++ src/ConvertTextInAudio/ConvertTextInAudio/Worker.cs
@@ -1,4 +1,5 @@
 using ConvertTextInAudio.Interfaces;
+using System.Diagnostics;
 
 namespace ConvertTextInAudio;
 public class Worker(
@@ -8,13 +9,21 @@ public class Worker(
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var stopwatch = new Stopwatch();
+
         try
         {
-            logger.LogInformation("Iniciando geração de áudio");
-            var audioBytes = await service.GenerateAudioAsync("Feliz Natal", stoppingToken);
+            logger.LogInformation("Iniciando geração de áudio...");
+            stopwatch.Start();
 
-            logger.LogInformation("Áudio gerado com sucesso");
-            await File.WriteAllBytesAsync("voz.mp3", audioBytes, stoppingToken);
+            var audioBytes = await service.GenerateAudioAsync("", stoppingToken);
+
+            logger.LogInformation("Áudio gerado com sucesso em {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+
+            var outputPath = "records/voz.mp3";
+            await File.WriteAllBytesAsync(outputPath, audioBytes, stoppingToken);
+
+            logger.LogInformation("Arquivo salvo em: {OutputPath}", outputPath);
         }
         catch (Exception ex)
         {
